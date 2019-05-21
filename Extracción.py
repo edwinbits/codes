f9cf2e202852f4ff1ff969c70bd35f481f72a637
edwinbits <46546549+edwinbits@users.noreply.github.com>
2019-05-20 19:50:55 -0500

    Update Extracción.py

--- Extracción.py
+++ Extracción.py
@@ -1,15 +1,19 @@
-#prueba en Python con extraccion de datos. Prueba para realizar experimento "Social". Caso: Video UTP
+#prueba en Python con extraccion de datos.
 
+#Libreria Tweepy de python permite utilizar las apis de Twitter para la mineria de datos
 import tweepy
 
-consumer_key= "8MUlhLXlsggAFYC1VyzlaaD5q"
-consumer__key_secret="ozLRT6AqbV1EJG4esvpxpENVgXb7LqABT3o7pO8vvG9ytRXnjo"
-access_token="1129264063064805376-eQpp7FafDtob4eiWuaUsICDSiFeJUW"
-access_token_secret="sjmPulB64Cs6R85o0nC1QMKATMPEXTrmQeVunsfwLOeJ0"
+#La consumer key(Api Key), consumer key secret, access token y access token secret se consiguen cuando creas un aplicacion en twitter
+consumer_key= "XXXXXXXXXXXXXXXXX"
+consumer__key_secret="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+access_token="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+access_token_secret="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
+#estas llaves y tokens se utilizan para autenticar al usuario por motivos de seguridad
 autenticar= tweepy.OAuthHandler(consumer_key,consumer__key_secret)
 autenticar.set_access_token(access_token,access_token_secret)
 
+#se crea un variable llamada api que almecena la autenticacion 
 api = tweepy.API(autenticar)
 
 #----------------------------------------------------------------------
@@ -18,11 +22,12 @@ api = tweepy.API(autenticar)
 username = "elonmusk"
 
 # número de tweets
-
 tweetCount = 20
 
+#La funcion timeline nos permite ver los tweets del usuario elegido
 resultado = api.user_timeline(id=username, count=tweetCount)
 
 print("username" + "     "+ "tweet")
+
 for tweet in resultado:
     print(username + "     "+ tweet.text)
